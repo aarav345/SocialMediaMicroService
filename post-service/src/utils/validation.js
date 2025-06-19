b10ab5c80cf1546260c88cd90b4443b2b0a25eda
aarav <aaravpradhan12365@gmail.com>
2025-06-19 19:39:20 +0545

    added mediaIds in validation in post-service

--- post-service/src/utils/validation.js
+++ post-service/src/utils/validation.js
@@ -4,6 +4,7 @@ const Joi = require("joi");
 const validationCreatePost = (data) => {
     const schema = Joi.object({
         content: Joi.string().min(3).max(5000).required(),
+        mediaIds: Joi.array()
     })
 
     return schema.validate(data);   
